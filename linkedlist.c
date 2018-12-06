517fc7df3294cd7d829ed5e6cd60b045b4e5dfaf
ewhaqh9909 <43231975+ewhaqh9909@users.noreply.github.com>
2018-12-06 15:40:44 +0900

    4

--- linkedlist.c
+++ linkedlist.c
@@ -11,3 +11,17 @@ typedef struct linknd{
 
 static linknd_t *list; //linked list ½ÇÃ¼ 
  
+ 
+ linknd_t* create_node(int value){
+ 	linknd_t* ndPtr;
+ 	ndPtr=(linknd_t*)malloc(sizeof(linknd_t));
+ 	if(ndPtr==NULL)
+ 	{
+ 		printf("ERROR\n");
+ 		return NULL;
+	 }
+ 	ndPtr->data=value;
+ 	ndPtr->next-NULL;
+	 
+	 return ndPtr; 
+ }
